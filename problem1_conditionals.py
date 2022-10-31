955494e4949166226940e51f434812d2437d9a6b
Vishnu Venkatesh <vish.venkatesh@outlook.my>
2022-10-31 19:29:37 +0800

    adding Conditionals Homework Problem 1

---
+++ problem1_conditionals.py
@@ -0,0 +1,10 @@
+time = 8 # time is an integer between 0 and 23
+
+if time < 9:
+    print("Morning is wonderful. Its only drawback is that it comes at such an inconvenient time of the day")
+elif time < 8:
+    print("Working hard or hardly working?")
+elif time < 22:
+    print("A day without sunshine is like, you know, night")
+elif time > 22:
+    print("burning the midnight oil!")
